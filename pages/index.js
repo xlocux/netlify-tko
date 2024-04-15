f5b85501818f6929b05807ff8ac29aa6edbb5e1f
Locu <xlocux@users.noreply.github.com>
2024-04-15 09:38:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Netlify Take Over!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Contact locu@pm.me</code>
         </p>
       </main>
 
